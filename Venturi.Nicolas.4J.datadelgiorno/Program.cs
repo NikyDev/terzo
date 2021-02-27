816427bb110f66526a5a5bedd1b64d62ed74c1e1
NikyDev <66072452+NikyDev@users.noreply.github.com>
2021-02-27 10:24:45 +0100

    primo commit

---
+++ Venturi.Nicolas.4J.datadelgiorno/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Venturi.Nicolas._4J.datadelgiorno.models;
+
+namespace Venturi.Nicolas._4J.datadelgiorno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s;
+            Data d = new Data(1, 1, 2003);
+            Data r = new Data(2, 6, 2006);
+            Data z = new Data();
+
+            d.aggiungigiorni(95,d);
+            //d.togligiorni(18, d);
+            //Console.WriteLine(d.giorno + "/" + d.mese + "/" + d.anno);
+            Console.WriteLine(d == r);
+            Console.WriteLine(d > r);
+            s= d.ToString();
+            Console.WriteLine(s);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Venturi.Nicolas.4J.datadelgiorno/models/Class1.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Venturi.Nicolas._4J.datadelgiorno.models
+{
+    class Data
+    {
+        private int _giorno;
+        private int _mese;
+        private int _anno;
+
+        public int giorno
+        {
+            get
+            {
+                return _giorno;
+            }
+            set
+            {
+                _giorno = value;
+
+            }
+        }
+
+        public int mese
+        {
+            get
+            {
+                return _mese;
+            }
+            set
+            {
+                _mese = value;
+
+
+            }
+        }
+
+        public int anno
+        {
+            get
+            {
+                return _anno;
+            }
+            set
+            {
+                _anno = value;
+            }
+        }
+
+        public Data(int g, int m, int a)
+        {
+            anno = a;
+            giorno = g;
+            mese = m;
+        }
+
+        public Data()
+        {
+            anno = 1900;
+            giorno = 1;
+            mese = 1;
+        }
+
+        public Data aggiungigiorni(int gagg, Data z)
+        {
+            int r = z.giorno;
+            z.giorno += gagg;
+            int f = (z.giorno % 31);
+            if (z.giorno > 31)
+            {
+                
+                z.mese += f;
+                z.giorno = z.giorno-(31*f)-r ;
+
+
+            }
+            if (z.mese > 12)
+            {
+                z.anno += 1;
+                z.mese = 1;
+            }
+
+
+            return z;
+        }
+        public Data togligiorni(int gtog, Data z)
+        {
+            z.giorno -= gtog;
+
+            if (z.giorno < 1)
+            {
+                z.mese -= 1;
+                z.giorno = z.giorno*-1;
+            }
+            if(z.giorno==0)
+            {
+                z.giorno = 31;
+            }
+            if (z.mese < 1)
+            {
+                z.anno -= 1;
+                z.mese = 12;
+            }
+            return z;
+        }
+
+        public static string operator == (Data f,Data g )
+            {
+            if(f.giorno==g.giorno)
+            {
+                if(f.mese==g.mese)
+                {
+                    if(f.anno==g.anno)
+                    {
+                        return "le due dato sono uguali";
+                    }
+                }
+            }
+
+            return "le due date sono diverse";
+            }
+
+        public static string operator !=(Data f, Data g)
+        {
+
+            return "si";
+        }
+
+        public static string operator > (Data f, Data g)
+        {
+            if(f.anno > g.anno)
+            {
+                return "la prima data è posteriore all'altra";
+            }
+            else
+            {
+                if(f.anno == g.anno)
+                {
+                    if(f.mese > g.mese)
+                    {
+                        return "la prima data è posteriore all'altra";
+                    }
+                    else
+                    {
+                        if (f.mese == g.mese)
+                        {
+                            if (f.giorno > g.giorno)
+                            {
+                                return "la prima data è posteriore all'altra";
+                            }
+                            else
+                            {
+                                if (f.giorno == g.giorno)
+                                {
+                                    return "le due date sono uguali";
+                                }
+                                else
+                                {
+                                    return "la seconda data è posteriore alla prima";
+                                }
+                            }
+                        }  
+                        else
+                        {
+                            return "la seconda data è posteriore alla prima";
+                        }
+                    }
+                }
+                else
+                {
+                    return "la seconda data è posteriore alla prima";
+                }
+            }
+
+        }
+
+        public static string operator < (Data f, Data g)
+        {
+            return "";
+        }
+
+        public override string ToString()
+        {
+
+            return giorno+"/"+mese+"/"+anno;
+        }
+
+
+    }
+}
